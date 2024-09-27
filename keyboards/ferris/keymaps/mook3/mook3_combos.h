867a67292daa01e0f89223dc7472e2410bd77eba
Mark Peters <mark.a.peters9@gmail.com>
2024-09-27 00:34:55 -0500

    Add more mod-loading combos (all non-Fn ones now)

--- keyboards/ferris/keymaps/mook3/mook3_combos.h
+++ keyboards/ferris/keymaps/mook3/mook3_combos.h
@@ -4,90 +4,61 @@
 #include "mook3_layers.h"
 
 enum combos {
-	/*
-    COMBO_LCTL,
-    COMBO_LALT,
-	COMBO_LGUI,
-	COMBO_LCTL_ALT,
-    COMBO_RCTL,
-    COMBO_RALT,
-	COMBO_RGUI,
-	COMBO_RCTL_ALT,
-	*/
-	
-	COMBO_LCTL_SFT,
-	COMBO_LCTL_SFT_ALT,
-	COMBO_LGUI2,
-	COMBO_LGUI_SFT,
-	COMBO_LALT2,
-	COMBO_LALT_SFT,
 	COMBO_FN,
+	COMBO_G,
+	COMBO_GC,
+	COMBO_A,
+	COMBO_AC,
+	COMBO_GA,
+	COMBO_GAC,
 	
-	COMBO_LALT_SFT2,
-	COMBO_LGUI_SFT2,
-	COMBO_LCTL_SFT2,
+	COMBO_CS,
+	COMBO_GS,
+	COMBO_GCS,
+	COMBO_AS,
+	COMBO_ACS, // MEH
+	COMBO_GAS,
+	COMBO_GACS, // HYPR
 
     COMBO_LENGTH // nifty trick to avoid manually specifying how many combos you have
 };
-uint16_t MOD_LOADING_COMBOS_START_IDX = COMBO_LCTL_SFT;
+uint16_t MOD_LOADING_COMBOS_START_IDX = COMBO_FN;
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-/*
-const uint16_t PROGMEM combo_fd[] = {KC_F, KC_D, COMBO_END};
-const uint16_t PROGMEM combo_fs[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_fa[] = {KC_F, KC_A, COMBO_END};
-const uint16_t PROGMEM combo_fds[] = {KC_F, KC_D, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_jk[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_jl[] = {KC_J, KC_L, COMBO_END};
-const uint16_t PROGMEM combo_jq[] = {KC_J, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM combo_jkl[] = {KC_J, KC_K, KC_L, COMBO_END};
-*/
-
 const uint16_t PROGMEM combo_fctl[] = {KC_F, CTL_TAB, COMBO_END};
-const uint16_t PROGMEM combo_rctl[] = {KC_R, CTL_TAB, COMBO_END};
+//const uint16_t PROGMEM combo_rctl[] = {KC_R, CTL_TAB, COMBO_END};
 const uint16_t PROGMEM combo_dctl[] = {KC_D, CTL_TAB, COMBO_END};
 const uint16_t PROGMEM combo_ectl[] = {KC_E, CTL_TAB, COMBO_END};
 const uint16_t PROGMEM combo_sctl[] = {KC_S, CTL_TAB, COMBO_END};
 const uint16_t PROGMEM combo_wctl[] = {KC_W, CTL_TAB, COMBO_END};
 const uint16_t PROGMEM combo_actl[] = {KC_A, CTL_TAB, COMBO_END};
+const uint16_t PROGMEM combo_qctl[] = {KC_Q, CTL_TAB, COMBO_END};
 
-const uint16_t PROGMEM combo_ssft[] = {KC_S, SFT_BSP, COMBO_END};
-const uint16_t PROGMEM combo_dsft[] = {KC_D, SFT_BSP, COMBO_END};
 const uint16_t PROGMEM combo_fsft[] = {KC_F, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_dsft[] = {KC_D, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_esft[] = {KC_E, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_ssft[] = {KC_S, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_wsft[] = {KC_W, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_asft[] = {KC_A, SFT_BSP, COMBO_END};
+const uint16_t PROGMEM combo_qsft[] = {KC_Q, SFT_BSP, COMBO_END};
+
 
 combo_t key_combos[] = {
-	// One-shot mods
-	/*
-	[COMBO_LCTL] = COMBO(combo_fd, OSM(MOD_LCTL)),
-	[COMBO_LALT] = COMBO(combo_fs, OSM(MOD_LALT)),
-	[COMBO_LGUI] = COMBO(combo_fa, OSM(MOD_LGUI)),
-	[COMBO_LCTL_ALT] = COMBO(combo_fds, OSM(MOD_LCTL | MOD_LALT)),
-	[COMBO_RCTL] = COMBO(combo_jk, OSM(MOD_RCTL)),
-	[COMBO_RALT] = COMBO(combo_jl, OSM(MOD_RALT)),
-	[COMBO_RGUI] = COMBO(combo_jq, OSM(MOD_RGUI)),
-	[COMBO_RCTL_ALT] = COMBO(combo_jkl, OSM(MOD_RCTL | MOD_RALT)),
-	*/
-	
-	[COMBO_LCTL_SFT] = COMBO(combo_fctl, MO(_FN)),
-	[COMBO_LCTL_SFT_ALT] = COMBO(combo_rctl, LSFT(LCTL(KC_LALT))),
-	[COMBO_LGUI2] = COMBO(combo_dctl, KC_LGUI),
-	[COMBO_LGUI_SFT] = COMBO(combo_ectl, LSFT(KC_LGUI)),
-	[COMBO_LALT2] = COMBO(combo_sctl, KC_LALT),
-	[COMBO_LALT_SFT] = COMBO(combo_wctl, LSFT(KC_LALT)),
-	[COMBO_FN] = COMBO(combo_actl, MO(_FN)),
-	
-	[COMBO_LALT_SFT2] = COMBO(combo_ssft, LSFT(KC_LALT)),
-	[COMBO_LGUI_SFT2] = COMBO(combo_dsft, LSFT(KC_LGUI)),
-	[COMBO_LCTL_SFT2] = COMBO(combo_fsft, LSFT(KC_LCTL)),
-	// Normal mods
-	/*[COMBO_LCTL] = COMBO(combo_fd, KC_LCTL),
-	[COMBO_LALT] = COMBO(combo_fs, KC_LALT),
-	[COMBO_LGUI] = COMBO(combo_fa, KC_LGUI),
-	[COMBO_LCTL_ALT] = COMBO(combo_fds, LCTL(KC_LALT)),
-	[COMBO_RCTL] = COMBO(combo_jk, KC_RCTL),
-	[COMBO_RALT] = COMBO(combo_jl, KC_RALT),
-	[COMBO_RGUI] = COMBO(combo_jq, KC_RGUI),
-	[COMBO_RCTL_ALT] = COMBO(combo_jkl, RCTL(KC_RALT)),*/
+	[COMBO_FN] = COMBO(combo_fctl, MO(_FN)),
+	[COMBO_G] = COMBO(combo_dctl, KC_LGUI),
+	[COMBO_GC] = COMBO(combo_ectl, C(KC_LGUI)),
+	[COMBO_A] = COMBO(combo_sctl, KC_LALT),
+	[COMBO_AC] = COMBO(combo_wctl, C(KC_LALT)),
+	[COMBO_GA] = COMBO(combo_actl, A(KC_LGUI)),
+	[COMBO_GAC] = COMBO(combo_qctl, C(A(KC_LGUI))),
+
+	[COMBO_CS] = COMBO(combo_fsft, S(KC_LCTL)),
+	[COMBO_GS] = COMBO(combo_dsft, S(KC_LGUI)),
+	[COMBO_GCS] = COMBO(combo_esft, S(C(KC_LGUI))),
+	[COMBO_AS] = COMBO(combo_ssft, S(KC_LALT)),
+	[COMBO_ACS] = COMBO(combo_wsft, KC_MEH),
+	[COMBO_GAS] = COMBO(combo_actl, S(A(KC_LGUI))),
+	[COMBO_GACS] = COMBO(combo_qctl, KC_HYPR),
 };
 
 bool is_mod_loading_combo(uint16_t index) {
@@ -113,7 +84,7 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
 	if (layer_state_is(_GAMING)) {
 		return false;
 	}
-	if (combo_index != COMBO_LCTL_SFT2 && timer_elapsed(custom_idle_timer) < 150) {
+	if (combo_index != COMBO_CS && timer_elapsed(custom_idle_timer) < 150) {
 		// Keyboard must have been idle for configured amount of time for combos to work.
 		// This disables combo when fast typing, mostly to remove input delay by allowing
 		// keys to be registered on keydown rather than keyup.
